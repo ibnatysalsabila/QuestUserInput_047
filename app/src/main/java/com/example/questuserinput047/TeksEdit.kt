74781bdd1ffd5272d4e164890d81c5667490ff37
ibnatysalsabila <salsajpr01@gmail.com>
2025-10-31 15:51:18 +0700

    membuat input untuk alamat

--- app/src/main/java/com/example/questuserinput047/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput047/TeksEdit.kt
@@ -71,3 +71,12 @@ fun FormDataDiri(modifier: Modifier
                 }
             }
         }
+        OutlinedTextField(
+            value = textAlamat,
+            singleLine = true,
+            modifier = Modifier.width(width=250.dp),
+            label = { Text(text = "Alamat Lengkap") },
+            onValueChange = {
+                textAlamat = it
+            }
+        )
