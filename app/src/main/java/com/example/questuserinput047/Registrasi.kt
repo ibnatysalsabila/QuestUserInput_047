eb0f850b06e6093353be2ea382b155c20c26fc83
ibnatysalsabila <salsajpr01@gmail.com>
2025-11-02 09:20:34 +0700

    menambahkan tombol submit yang muncul ketika checkbox di centang

--- app/src/main/java/com/example/questuserinput047/Registrasi.kt
+++ app/src/main/java/com/example/questuserinput047/Registrasi.kt
@@ -236,3 +236,23 @@ fun FormRegistrasi(modifier: Modifier = Modifier) {
                     Text("Saya setuju dengan syarat & ketentuan")
                 }
 
+                Button(
+                    onClick = {
+                        showDialog = true
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 20.dp)
+                        .height(50.dp)
+                        .clip(RoundedCornerShape(20.dp)),
+                    enabled = isChecked,
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = if (isChecked) Color(0xFF1565C0) else Color.LightGray
+                    )
+                ) {
+                    Text("Submit", color = Color.White)
+                }
+            }
+        }
+    }
+
