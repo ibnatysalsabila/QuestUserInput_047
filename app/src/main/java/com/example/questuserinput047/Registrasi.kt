f2a2fc011f0602f8c9f53e8b0e556e91c806aa23
ibnatysalsabila <salsajpr01@gmail.com>
2025-11-02 09:00:42 +0700

    menambahkan variabel untuk menyimpan data input

---
+++ app/src/main/java/com/example/questuserinput047/Registrasi.kt
@@ -0,0 +1,45 @@
+package com.example.questuserinput047
+
+import android.app.DatePickerDialog
+import android.widget.DatePicker
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.PressInteraction
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import java.util.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun FormRegistrasi(modifier: Modifier = Modifier) {
+
+    var nama by remember { mutableStateOf("") }
+    var kotaAsal by remember { mutableStateOf("") }
+    var tanggalLahir by remember { mutableStateOf("") }
+    var rt by remember { mutableStateOf("") }
+    var rw by remember { mutableStateOf("") }
+    var umur by remember { mutableStateOf("") }
+    var gender by remember { mutableStateOf("") }
+    var isChecked by remember { mutableStateOf(false) }
+
+    var showDialog by remember { mutableStateOf(false) }
+
+    val genderOptions = listOf("Laki-laki", "Perempuan")
+
+    }
