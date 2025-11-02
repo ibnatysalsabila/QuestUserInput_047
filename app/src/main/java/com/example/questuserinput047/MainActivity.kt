88ecd57e46cad82ea869b5708ab0556d02f54b85
ibnatysalsabila <salsajpr01@gmail.com>
2025-11-02 09:23:12 +0700

    memanggil fungsi formregistrasi pada main activity

--- app/src/main/java/com/example/questuserinput047/MainActivity.kt
+++ app/src/main/java/com/example/questuserinput047/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestUserInput047Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    FormDataDiri(
+                    FormRegistrasi(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
